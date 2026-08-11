8048176c9076e775c3c8f0a40a9db12592c0ac9b
raaz4n <raazandoski@gmail.com>
2026-08-11 12:42:17 -0500

    added custom user ms timing

---
+++ r3turn.go
@@ -0,0 +1,62 @@
+// r3turn by raaz4n
+// The usage for this program is ./r3turn <file name> <request time in milliseconds>
+// It is most efficient when used alongside subfinder.
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: ./r3turn <file> <request time in ms>")
+		return
+	}
+
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println("Error reading file: ", err)
+		return
+	}
+
+	defer file.Close()
+
+	timeMs, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Println("Invalid ms value: ", err)
+		return
+	}
+
+	client := &http.Client{
+		Timeout: time.Duration(timeMs) * time.Millisecond,
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		// check if the string contains https://
+		if !strings.Contains(line, "https://") {
+			line = "https://" + line
+		}
+
+		resp, err := client.Get(line)
+		if err != nil {
+			fmt.Printf("Failed to send req to %s\n", line)
+			continue
+		}
+
+		defer resp.Body.Close()
+
+		status := resp.Status
+
+		fmt.Printf("[%s] %s\n", status, line)
+	}
+}
